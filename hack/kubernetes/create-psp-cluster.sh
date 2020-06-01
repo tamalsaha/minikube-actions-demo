eca69151c3ed5e97472367b3e090e995f12cbaf3
Tamal Saha <tamal@appscode.com>
2020-06-01 13:32:21 -0700

    added-all

---
+++ hack/kubernetes/create-psp-cluster.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -xeou pipefail
+
+K8S_VERSION=${1:-v1.18.3}
+
+minikube start \
+  --driver=none \
+  --kubernetes-version=$K8S_VERSION \
+  --wait=none \
+  --extra-config=apiserver.enable-admission-plugins="NamespaceLifecycle,LimitRanger,ServiceAccount,TaintNodesByCondition,Priority,DefaultTolerationSeconds,DefaultStorageClass,StorageObjectInUseProtection,PersistentVolumeClaimResize,RuntimeClass,CertificateApproval,CertificateSigning,CertificateSubjectRestriction,DefaultIngressClass,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota,PodSecurityPolicy"
+
+echo "Waiting kubernetes to launch on 8443..."
+while ! nc -z localhost 8443; do
+  sleep 1 # wait for 1/10 of the second before check again
+done
+sleep 5
+
+kubectl version --short
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/privileged-psp.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/baseline-psp.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/restricted-psp.yaml
+
+kubectl apply -f ./hack/kubernetes/psp-rbac.yaml
--- hack/kubernetes/psp/create-psp-cluster.sh
+++ hack/kubernetes/psp/create-psp-cluster.sh
@@ -10,18 +10,13 @@ minikube start \
 echo "Waiting kubernetes to launch on 8443..."
 while ! nc -z localhost 8443; do
   sleep 1 # wait for 1/10 of the second before check again
-  echo ">>>>>>>>>>>>>>>>>>> L 12"
 done
 sleep 5
 
 kubectl version --short
 
-echo ">>>>>>>>>>>>>>>>>>> L 18"
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/privileged-psp.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/baseline-psp.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/website/master/content/en/examples/policy/restricted-psp.yaml
 
-echo ">>>>>>>>>>>>>>>>>>> L 23"
-kubectl apply -f ./hack/kubernetes/psp/cluster-roles.yaml
-echo ">>>>>>>>>>>>>>>>>>> L 25"
-kubectl apply -f ./hack/kubernetes/psp/role-bindings.yaml
+kubectl apply -f ./hack/kubernetes/psp-rbac.yaml
